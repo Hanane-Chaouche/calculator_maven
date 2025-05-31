8de443c3ba64f4cdc9f2e7c05de0e87a489280bc
hanane-py <6169778@bdeb.qc.ca>
2025-05-31 15:55:22 -0400

    s

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,33 @@
+package com.example;
+
+public class Calculator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+
+package com.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero.");
+        }
+        return (double) a / b;
+    }
+}
